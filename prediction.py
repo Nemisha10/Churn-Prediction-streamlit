3dfc81d394efcb9ae992176a90c680cf4799727c
Nemisha <104445880+Nemisha10@users.noreply.github.com>
2023-08-25 18:19:53 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -13,7 +13,10 @@ joblib_file = "joblib_knn_model.pkl"
 joblib_knn_model = joblib.load(joblib_file)
 joblib.dump(joblib_knn_model, joblib_file)
 
-def forecast(data):
+x_sample = [[80005, 56, 7, 71.54, 246, False, True],
+       [80006, 40, 4, 33.71, 336, False, True]]
+
+def forecast(data=x_sample):
  joblib_knn_model = joblib.load(joblib_file)
  return joblib_knn_model.predict(data)
 
